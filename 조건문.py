4b404e59ab08fd88880030442aa15cdf26bed5a8
seunghyun9999 <sean1177@naver.com>
2024-07-10 17:09:04 +0900

    조건문
    조건문배움

---
+++ 조건문.py
@@ -0,0 +1,35 @@
+# if 3>1 :
+#     print('3은 1보다 큽니다')
+#     print('연산을 종료합니다.')
+# print("-"*30)
+# print('저는 들여쓰기를 하지 않은 라인입니다.')
+#
+# a = '전승현'
+# b = '이관훈'
+#
+# if a==b :
+#     print(a+'랑 '+b+'는 같습니다.')
+# elif a!=b :
+#     print(a+'랑 '+b+'는 다릅니다.')
+#
+# num_1 = 10
+# num_2 = 3
+# if num_1<num_2 :
+#     print('작아요')
+# elif num_1>num_2 :
+#     print('커요')
+# elif num_1==num_2 :
+#     print('같아요')
+
+# 퀴즈에도 배운게 있음
+
+# apple ='사과'
+# banana = '바나나'
+# if apple == '사과' or banana =='바나나':
+#     print('문자열이 모두 정확합니다.')
+    #틀림
+
+var = [1,2,3,4,5]
+if 2 in var :
+    print(2,"가 var안에 있음")
+
